cb636c9fe6b61c39e057ee12c3b5c82e954d3a06
MaksymPavliuk <mpavliuk@griddynamics.com>
2024-12-17 11:21:39 +0200

    Solved task 5

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,47 @@
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 public class Main {
     static class User {
-
         private String name;
-
         private int age;
 
-
         public User(String name, int age) {
-
             this.name = name;
-
             this.age = age;
-
         }
 
         public String getName() {
-
             return name;
-
         }
 
         public void setName(String name) {
-
             this.name = name;
-
         }
 
         public int getAge() {
-
             return age;
-
         }
 
         public void setAge(int age) {
-
             this.age = age;
-
         }
 
+        @Override
+        public boolean equals(Object o) {
+            if (o == null || getClass() != o.getClass()) return false;
+            User user = (User) o;
+            return age == user.age && Objects.equals(name, user.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name, age);
+        }
     }
 
     public static void main(String[] args) {
-
         Set<User> users = new HashSet<>();
         users.add(new User("Max", 27));
         users.add(new User("Veronika", 20));
